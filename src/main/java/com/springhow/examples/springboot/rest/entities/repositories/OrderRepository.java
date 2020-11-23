07dd464141abc1edd8c7e7a59ed51a1bba6c3769
Raja Anbazhagan <raja.anbazhagan1991@gmail.com>
2020-11-23 22:13:57 +0530

    implemented restful repositories

--- src/main/java/com/springhow/examples/springboot/rest/entities/repositories/OrderRepository.java
+++ src/main/java/com/springhow/examples/springboot/rest/entities/repositories/OrderRepository.java
@@ -2,9 +2,11 @@ package com.springhow.examples.springboot.rest.entities.repositories;
 
 import com.springhow.examples.springboot.rest.entities.OrderHeader;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RepositoryRestResource(collectionResourceRel = "orders",itemResourceRel = "order", path = "orders")
 public interface OrderRepository extends JpaRepository<OrderHeader, Integer> {
 
 }
